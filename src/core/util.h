6516c7cc6fa2b250d2ea635a5b820ea7c65e77d9
theonlynavin <navinkumar.lakshmanaraj@gmail.com>
2024-04-19 22:49:46 +0530

    added more math functions

--- src/core/util.h
+++ src/core/util.h
@@ -15,6 +15,36 @@ namespace tinge
 
     typedef float scalar;
 
+    __host__ __device__ static inline vec2 make_vec2(scalar x)
+    {
+        return vec2{x, x};
+    }
+
+    __host__ __device__ static inline vec2 make_vec2(scalar x, scalar y)
+    {
+        return vec2{x, y};
+    }
+
+    __host__ __device__ static inline vec3 make_vec3(scalar x)
+    {
+        return vec3{x, x, x};
+    }
+
+    __host__ __device__ static inline vec3 make_vec3(scalar x, scalar y, scalar z)
+    {
+        return vec3{x, y, z};
+    }
+
+    __host__ __device__ static inline vec4 make_vec4(scalar x)
+    {
+        return vec4{x, x, x, x};
+    }
+
+    __host__ __device__ static inline vec4 make_vec4(scalar x, scalar y, scalar z, scalar w)
+    {
+        return vec4{x, y, z, w};
+    }
+
     /* Vector addition */
 
     __host__ __device__ static inline vec2 operator+(const vec2& a, const vec2& b)
@@ -283,21 +313,56 @@ namespace tinge
         return a * invlen;
     }
 
+    /* Quaternion */
+
+    typedef float4 quatn;
+
+    __host__ __device__ static inline quatn conjugate(const quatn& q)
+    {
+        return quatn{-q.x, -q.y, -q.z, q.w};
+    }
+
+    __host__ __device__ static inline quatn axis_angle(const vec3& axis, scalar angle)
+    {
+        scalar halfAngle = 0.5 * angle;
+        scalar sha = sin(halfAngle);
+
+        return quatn{axis.x * sha, axis.y * sha, axis.z * sha, cos(halfAngle)};
+    }
+
+    __host__ __device__ static inline quatn euler_angles(scalar u, scalar v, scalar w)
+    {
+        scalar u2 = 0.5 * u, v2 = 0.5 * v, w2 = 0.5 * w;
+        scalar su2 = sin(u2), sv2 = sin(v2), sw2 = sin(w2);
+        scalar cu2 = cos(u2), cv2 = cos(v2), cw2 = cos(w2);
+
+        return quatn{
+        su2 * cv2 * cw2 - cu2 * sv2 * sw2,
+        cu2 * sv2 * cw2 + su2 * cv2 * sw2,
+        cu2 * cv2 * sw2 - su2 * sv2 * cw2,
+        cu2 * cv2 * cw2 + su2 * sv2 * sw2};
+    }
+
     /* Matrix operations */
 
-    struct matrix3
+    struct mat3
     {
-        float3 m[3];
+        float3 m[3] = {float3{1, 0, 0}, 
+                    float3{0, 1, 0}, 
+                    float3{0, 0, 1}};    // rows of the matrix
     };
 
-    struct matrix4
+    struct mat4
     {
-        float4 m[4];
+        float4 m[4] = {float4{1, 0, 0, 0}, 
+                    float4{0, 1, 0, 0}, 
+                    float4{0, 0, 1, 0}, 
+                    float4{0, 0, 0, 1}};    // rows of the matrix
     };
     
-    __host__ __device__ static inline matrix3 transpose(const matrix3& m)
+    __host__ __device__ static inline mat3 transpose(const mat3& m)
     {
-        matrix3 transpose;
+        mat3 transpose;
         transpose.m[0].x = m.m[0].x;
         transpose.m[0].y = m.m[1].x;
         transpose.m[0].z = m.m[2].x;
@@ -310,9 +375,9 @@ namespace tinge
         return transpose;
     }
 
-    __host__ __device__ static inline matrix4 transpose(const matrix4& m)
+    __host__ __device__ static inline mat4 transpose(const mat4& m)
     {
-        matrix4 transpose;
+        mat4 transpose;
         transpose.m[0].x = m.m[0].x;
         transpose.m[0].y = m.m[1].x;
         transpose.m[0].z = m.m[2].x;
@@ -332,7 +397,176 @@ namespace tinge
         return transpose;
     }
 
-    __host__ __device__ static inline vec3 operator*(const vec3& a, const matrix3& b)
+    /* Matrix addition */
+
+    __host__ __device__ static inline mat3 operator+(const mat3& a, const mat3& b)
+    {
+        mat3 m;
+
+        m.m[0] = a.m[0] + b.m[0];
+        m.m[1] = a.m[1] + b.m[1];
+        m.m[2] = a.m[2] + b.m[2];
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 operator+(const mat4& a, const mat4& b)
+    {
+        mat4 m;
+
+        m.m[0] = a.m[0] + b.m[0];
+        m.m[1] = a.m[1] + b.m[1];
+        m.m[2] = a.m[2] + b.m[2];
+        m.m[3] = a.m[3] + b.m[3];
+
+        return m;
+    }
+
+    /* Matrix subtraction */
+
+    __host__ __device__ static inline mat3 operator-(const mat3& a, const mat3& b)
+    {
+        mat3 m;
+
+        m.m[0] = a.m[0] - b.m[0];
+        m.m[1] = a.m[1] - b.m[1];
+        m.m[2] = a.m[2] - b.m[2];
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 operator-(const mat4& a, const mat4& b)
+    {
+        mat4 m;
+
+        m.m[0] = a.m[0] - b.m[0];
+        m.m[1] = a.m[1] - b.m[1];
+        m.m[2] = a.m[2] - b.m[2];
+        m.m[3] = a.m[3] - b.m[3];
+
+        return m;
+    }
+
+    /* Matrix multiplication */
+
+    __host__ __device__ static inline mat3 operator*(const mat3& a, const mat3& b)
+    {
+        mat3 m;
+
+        m.m[0].x = a.m[0].x * b.m[0].x + a.m[0].y * b.m[1].x + a.m[0].z * b.m[2].x;
+        m.m[0].y = a.m[0].x * b.m[0].y + a.m[0].y * b.m[1].y + a.m[0].z * b.m[2].y;
+        m.m[0].z = a.m[0].x * b.m[0].z + a.m[0].y * b.m[1].z + a.m[0].z * b.m[2].z;
+
+        m.m[1].x = a.m[1].x * b.m[0].x + a.m[1].y * b.m[1].x + a.m[1].z * b.m[2].x;
+        m.m[1].y = a.m[1].x * b.m[0].y + a.m[1].y * b.m[1].y + a.m[1].z * b.m[2].y;
+        m.m[1].z = a.m[1].x * b.m[0].z + a.m[1].y * b.m[1].z + a.m[1].z * b.m[2].z;
+
+        m.m[0].x = a.m[2].x * b.m[0].x + a.m[1].y * b.m[1].x + a.m[2].z * b.m[2].x;
+        m.m[0].y = a.m[2].x * b.m[0].y + a.m[1].y * b.m[1].y + a.m[2].z * b.m[2].y;
+        m.m[0].z = a.m[2].x * b.m[0].z + a.m[1].y * b.m[1].z + a.m[2].z * b.m[2].z;
+
+        return m;
+        
+    }
+
+    __host__ __device__ static inline mat4 operator*(const mat4& a, const mat4& b)
+    {
+        mat4 m;
+
+        m.m[0].x = a.m[0].x * b.m[0].x + a.m[0].y * b.m[1].x + a.m[0].z * b.m[2].x + a.m[0].w * b.m[3].x;
+        m.m[0].y = a.m[0].x * b.m[0].y + a.m[0].y * b.m[1].y + a.m[0].z * b.m[2].y + a.m[0].w * b.m[3].y;
+        m.m[0].z = a.m[0].x * b.m[0].z + a.m[0].y * b.m[1].z + a.m[0].z * b.m[2].z + a.m[0].w * b.m[3].z;
+        m.m[0].w = a.m[0].x * b.m[0].w + a.m[0].y * b.m[1].w + a.m[0].z * b.m[2].w + a.m[0].w * b.m[3].w;
+        
+        m.m[1].x = a.m[1].x * b.m[0].x + a.m[1].y * b.m[1].x + a.m[1].z * b.m[2].x + a.m[1].w * b.m[3].x;
+        m.m[1].y = a.m[1].x * b.m[0].y + a.m[1].y * b.m[1].y + a.m[1].z * b.m[2].y + a.m[1].w * b.m[3].y;
+        m.m[1].z = a.m[1].x * b.m[0].z + a.m[1].y * b.m[1].z + a.m[1].z * b.m[2].z + a.m[1].w * b.m[3].z;
+        m.m[1].w = a.m[1].x * b.m[0].w + a.m[1].y * b.m[1].w + a.m[1].z * b.m[2].w + a.m[1].w * b.m[3].w;
+
+        m.m[2].x = a.m[2].x * b.m[0].x + a.m[2].y * b.m[1].x + a.m[2].z * b.m[2].x + a.m[2].w * b.m[3].x;
+        m.m[2].y = a.m[2].x * b.m[0].y + a.m[2].y * b.m[1].y + a.m[2].z * b.m[2].y + a.m[2].w * b.m[3].y;
+        m.m[2].z = a.m[2].x * b.m[0].z + a.m[2].y * b.m[1].z + a.m[2].z * b.m[2].z + a.m[2].w * b.m[3].z;
+        m.m[2].w = a.m[2].x * b.m[0].w + a.m[2].y * b.m[1].w + a.m[2].z * b.m[2].w + a.m[2].w * b.m[3].w;
+
+        m.m[3].x = a.m[3].x * b.m[0].x + a.m[3].y * b.m[1].x + a.m[3].z * b.m[2].x + a.m[3].w * b.m[3].x;
+        m.m[3].y = a.m[3].x * b.m[0].y + a.m[3].y * b.m[1].y + a.m[3].z * b.m[2].y + a.m[3].w * b.m[3].y;
+        m.m[3].z = a.m[3].x * b.m[0].z + a.m[3].y * b.m[1].z + a.m[3].z * b.m[2].z + a.m[3].w * b.m[3].z;
+        m.m[3].w = a.m[3].x * b.m[0].w + a.m[3].y * b.m[1].w + a.m[3].z * b.m[2].w + a.m[3].w * b.m[3].w;
+
+        return m;
+        
+    }
+
+    __host__ __device__ static inline mat3 operator*(const mat3& a, scalar b)
+    {
+        mat3 m = a;
+
+        m.m[0] = m.m[0] * b;
+        m.m[1] = m.m[1] * b;
+        m.m[2] = m.m[2] * b;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 operator*(const mat4& a, scalar b)
+    {
+        mat4 m = a;
+
+        m.m[0] = m.m[0] * b;
+        m.m[1] = m.m[1] * b;
+        m.m[2] = m.m[2] * b;
+        m.m[3] = m.m[3] * b;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat3 operator/(const mat3& a, scalar b)
+    {
+        mat3 m = a;
+
+        m.m[0] = m.m[0] / b;
+        m.m[1] = m.m[1] / b;
+        m.m[2] = m.m[2] / b;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 operator/(const mat4& a, scalar b)
+    {
+        mat4 m = a;
+
+        m.m[0] = m.m[0] / b;
+        m.m[1] = m.m[1] / b;
+        m.m[2] = m.m[2] / b;
+        m.m[3] = m.m[3] / b;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat3 operator*(scalar a, const mat3& b)
+    {
+        mat3 m = b;
+
+        m.m[0] = m.m[0] * a;
+        m.m[1] = m.m[1] * a;
+        m.m[2] = m.m[2] * a;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 operator*(scalar a, const mat4& b)
+    {
+        mat4 m = b;
+
+        m.m[0] = m.m[0] * a;
+        m.m[1] = m.m[1] * a;
+        m.m[2] = m.m[2] * a;
+        m.m[3] = m.m[3] * a;
+
+        return m;
+    }
+
+    __host__ __device__ static inline vec3 operator*(const vec3& a, const mat3& b)
     {
         return vec3
         {
@@ -342,7 +576,7 @@ namespace tinge
         };
     }
 
-    __host__ __device__ static inline vec3 operator*(const matrix3& a, const vec3& b)
+    __host__ __device__ static inline vec3 operator*(const mat3& a, const vec3& b)
     {
         return vec3
         {
@@ -352,7 +586,7 @@ namespace tinge
         };
     }
 
-    __host__ __device__ static inline vec4 operator*(const vec4& a, const matrix4& b)
+    __host__ __device__ static inline vec4 operator*(const vec4& a, const mat4& b)
     {
         return vec4
         {
@@ -363,7 +597,7 @@ namespace tinge
         };
     }
 
-    __host__ __device__ static inline vec4 operator*(const matrix4& a, const vec4& b)
+    __host__ __device__ static inline vec4 operator*(const mat4& a, const vec4& b)
     {
         return vec4
         {
@@ -372,6 +606,64 @@ namespace tinge
             a.m[2].x * b.x + a.m[2].y * b.y + a.m[2].z * b.z + a.m[2].w * b.w,
             a.m[3].x * b.x + a.m[3].y * b.y + a.m[3].z * b.z + a.m[3].w * b.w
         };
+    }
+
+    /* Transformation functions */
+
+    __host__ __device__ static inline mat4 make_identity()
+    {
+        mat4 m;
+        m.m[0].x = 1;
+        m.m[1].y = 1;
+        m.m[2].z = 1;
+        m.m[3].w = 1;
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 make_rotation(const quatn& q)
+    {
+        mat4 m;
+        m.m[0].x = 1 - 2*q.y*q.y - 2*q.z*q.z;
+        m.m[0].y = 2*q.x*q.y - 2*q.w*q.z;
+        m.m[0].z = 2*q.x*q.z + 2*q.w*q.y;
+
+        m.m[1].x = 2*q.x*q.y + 2*q.w*q.z;
+        m.m[1].y = 1 - 2*q.x*q.x - 2*q.z*q.z;
+        m.m[1].z = 2*q.y*q.z - 2*q.w*q.x;
+
+        m.m[2].x = 2*q.x*q.z - 2*q.w*q.y;
+        m.m[2].y = 2*q.y*q.z + 2*q.w*q.x;
+        m.m[2].z = 1 - 2*q.x*q.x - 2*q.y*q.y;
+
+        m.m[3].w = 1;
+
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 make_scale(const vec3& scale)
+    {
+        mat4 m;
+        m.m[0].x = scale.x;
+        m.m[1].y = scale.y;
+        m.m[2].z = scale.z;
+        m.m[3].w = 1;
+        return m;
+    }
+
+    __host__ __device__ static inline mat4 make_translation(const vec3& translation)
+    {
+        mat4 m;
+        m.m[0].w = translation.x;
+        m.m[1].w = translation.y;
+        m.m[2].w = translation.z;
+        m.m[3].w = 1;
+        return m;
+    }
+
+
+    __host__ __device__ static inline mat4 make_transformation(const vec3& translation, const quatn& rotation, const vec3& scale)
+    {
+        return  make_translation(translation) * make_rotation(rotation) * make_scale(scale);
     }
 
     /* Output to stream */
@@ -397,7 +689,7 @@ namespace tinge
         return stream;
     }
 
-    __host__ static inline std::ostream& operator<< (std::ostream& stream, const matrix3& m)
+    __host__ static inline std::ostream& operator<< (std::ostream& stream, const mat3& m)
     {
         stream << "[" << m.m[0].x << ", " << m.m[0].y << ", " << m.m[0].z << "]\n";
         stream << "[" << m.m[1].x << ", " << m.m[1].y << ", " << m.m[1].z << "]\n";
@@ -406,7 +698,7 @@ namespace tinge
         return stream;
     }
 
-    __host__ static inline std::ostream& operator<< (std::ostream& stream, const matrix4& m)
+    __host__ static inline std::ostream& operator<< (std::ostream& stream, const mat4& m)
     {
         stream << "[" << m.m[0].x << ", " << m.m[0].y << ", " << m.m[0].z << ", " << m.m[0].w << "]\n";
         stream << "[" << m.m[1].x << ", " << m.m[1].y << ", " << m.m[1].z << ", " << m.m[1].w << "]\n";
