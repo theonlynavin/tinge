dc0026619190d34c19aaf4970ae60f49c75e646b
swamishiju <swaminathshiju24@gmail.com>
2025-03-07 23:27:13 +0530

    Bug fixes

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,25 +1,27 @@
-#include "math.h"
 #include "camera.h"
+#include "math.h"
 
-Ray :: Ray(Vec3 origin , Vec3 direction) : origin(origin), direction(normalize(direction)) {} // Parameterized constructor
+Ray ::Ray(Vec3 origin, Vec3 direction)
+    : origin(origin), direction(normalize(direction)) {
+} // Parameterized constructor
 // Direction must be normalized while taking in
-Ray :: ~Ray() {}
 
-Vec3 Ray::at(float t) {
+Vec3 Ray::at(float t) const {
     return origin + direction * t;
 } // Should return origin + t*direction;
 
-Camera :: Camera(float vertical_fov , int film_width , int film_height , float focal_length)
-: vertical_fov(vertical_fov), film_width(film_width), 
-film_height(film_height), focal_length(focal_length) {} 
+Camera ::Camera(float vertical_fov, int film_width, int film_height,
+                float focal_length)
+    : vertical_fov(vertical_fov), film_width(film_width),
+      film_height(film_height), focal_length(focal_length) {}
 
-Camera :: ~Camera() {}
+Camera ::~Camera() {}
 
-Ray Camera :: generate_ray(float u, float v) {
+Ray Camera ::generate_ray(float u, float v) {
 
     // Given NDC coordinates (u, v), should generate the corresponding ray
     float aspect_ratio = static_cast<float>(film_width) / film_height;
-    float half_height = (focal_length) * tan((vertical_fov) / 2);
+    float half_height = (focal_length)*tan((vertical_fov) / 2);
     float half_width = aspect_ratio * half_height;
 
     float x_coord = (2 * u - 1) * half_width;
@@ -28,6 +30,7 @@ Ray Camera :: generate_ray(float u, float v) {
     Vec3 origin(0, 0, 0); // All rays start from the pin-hole(assumed at origin)
     Vec3 direction(x_coord, y_coord, -focal_length);
     // Position vector for the given NDC{assumed range [0,1]}
-    
-    return Ray(frame.frameToWorld * origin, (frame.frameToWorld&direction).normalized());
 
+    return Ray(frame.frameToWorld * origin,
+               (frame.frameToWorld & direction).normalized());
+}
--- src/camera.h
+++ src/camera.h
@@ -1,32 +1,32 @@
 #pragma once
+#include "frame.h"
 #include "math.h"
-#include "Frame.h"
 struct Ray {
     Vec3 origin;
     Vec3 direction;
 
-    Ray(Vec3 origin , Vec3 direction);  // Parameterized constructor
+    Ray(Vec3 origin, Vec3 direction); // Parameterized constructor
     // Direction must be normalized while taking in
 
-    Vec3 at(float t) const; 
+    Vec3 at(float t) const;
     // Should return origin + t*direction;
-
 };
 class Camera {
-    public:
-        float vertical_fov;
-        int film_width , film_height;
-        float focal_length;
-        float z_near = 1e-6f, z_far = 1e6f;
+  public:
+    float vertical_fov;
+    int film_width, film_height;
+    float focal_length;
+    float z_near = 1e-6f, z_far = 1e6f;
 
-    Camera(float vertical_fov , int film_width , int film_height , float focal_length); 
+    Camera(float vertical_fov, int film_width, int film_height,
+           float focal_length);
     // Parameterized constructor
 
-    ~Camera (); // Destructor
+    ~Camera(); // Destructor
 
     Ray generate_ray(float u, float v);
     // Given NDC coordinates (u, v), should generate the corresponding ray
-    
+
     Frame frame;
     // Matrix containing transformation from world space to camera space
-}; 
+};
