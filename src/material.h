d7bf354c8710f8ee2e593a3e378a29dcd8384d0b
Navin <140264093+theonlynavin@users.noreply.github.com>
2025-03-19 20:37:29 +0530

    updated material.h
    added const identifiers wherever appropriate

--- src/material.h
+++ src/material.h
@@ -2,19 +2,17 @@
 #include "math.h"
 #include "camera.h"
 
-
 class AbstractMaterial{
 public:
     Vec3 color;
-    virtual Vec3 Fr(Ray wi, Ray wo,Vec3 n){
-    }
+    virtual Vec3 Fr(const Ray& wi, const Ray& wo, Vec3 n) const = 0;
 };
 
 class MaterialDiffuse : public AbstractMaterial{
 public:
-   Vec3 Fr(Ray wi, Ray wo,Vec3 n) override {
-    Vec3 return_col = color*dot(wi.direction,n);
-    return return_col;
+    Vec3 Fr(const Ray& wi, const Ray& wo, Vec3 n) const override {
+        Vec3 return_col = color*dot(wi.direction, n);
+        return return_col;
     }
 };
 
