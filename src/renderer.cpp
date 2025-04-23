219b1ecf4a418042476add6457b40aac4a9e9ae3
swamishiju <swaminathshiju24@gmail.com>
2025-04-23 21:33:59 +0530

    Ruski roulette

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,6 +2,7 @@
 #include "camera.h"
 #include "material.h"
 #include "math.h"
+#include "util.h"
 #include <ctime>
 #include <iostream>
 #include <memory>
@@ -23,7 +24,6 @@ void render_thread(Camera camera, const std::vector<obj_pointer> &shapes,
     Vec3 sky_blue = Vec3(0.1f, 0.5f, 0.9f);
     Vec3 sky_white = Vec3(1, 1, 1);
 
-        
     for (int i = w1; i < w2; i++) {
         counter_mutex.lock();
         if ((w2 - i) % (out_width / 100) == 0) {
@@ -39,27 +39,31 @@ void render_thread(Camera camera, const std::vector<obj_pointer> &shapes,
         for (int j = 0; j < out_height; j++) {
 
             int pix = out_width * j + i;
-            
+
             Vec3 color(0, 0, 0);
-            
+
             for (int sample = 0; sample < num_samples; sample++) {
 
-                v = 1 - (float)(j + 2 * random_generator.GenerateUniformFloat() - 1) / out_height;
-                u = (float)(i + 2 * random_generator.GenerateUniformFloat() - 1) / out_width;
+                v = 1 -
+                    (float)(j + 2 * random_generator.GenerateUniformFloat() -
+                            1) /
+                        out_height;
+                u = (float)(i + 2 * random_generator.GenerateUniformFloat() -
+                            1) /
+                    out_width;
 
                 const Ray ray = camera.generate_ray(u, v, random_generator);
                 auto hit = closestIntersect(shapes, ray);
 
                 IntersectionOut &details = hit.second;
-                
+
                 if (details.hit == true) {
-                        color = color + Renderer::illuminance(details, depth,
-                                                              shapes,
-                                                              random_generator);
-                }
-                else {
+                    color =
+                        color + Renderer::illuminance(details, depth, shapes,
+                                                      random_generator);
+                } else {
                     color = color + mix(sky_white, sky_blue, v);
-                    //color = color + Vec3(0, 0, 0);
+                    // color = color + Vec3(0, 0, 0);
                 }
             }
 
@@ -68,9 +72,9 @@ void render_thread(Camera camera, const std::vector<obj_pointer> &shapes,
             color = clamp(color, Vec3(0, 0, 0), Vec3(1, 1, 1));
 
             // Converting normalized RGB to 8-bit RGB
-            data[pix * 3 + 0] = (unsigned char)(255 * pow(color.x, 1/1.8));
-            data[pix * 3 + 1] = (unsigned char)(255 * pow(color.y, 1/1.8));
-            data[pix * 3 + 2] = (unsigned char)(255 * pow(color.z, 1/1.8));
+            data[pix * 3 + 0] = (unsigned char)(255 * pow(color.x, 1 / 1.8));
+            data[pix * 3 + 1] = (unsigned char)(255 * pow(color.y, 1 / 1.8));
+            data[pix * 3 + 2] = (unsigned char)(255 * pow(color.z, 1 / 1.8));
         }
     }
 }
@@ -96,13 +100,14 @@ void Renderer::render(Camera camera, const std::vector<obj_pointer> &shapes,
     std::vector<std::thread> threads;
     int N = 10;
     threads.reserve(N);
-    int num_samples = 50, depth = 4;
+    int num_samples = 30, depth = 4;
 
     // Each thread renders 1/10th width of scene
     for (int i = 0; i < N; i++) {
-        threads.emplace_back(std::thread(render_thread, camera, shapes, data, i * out_width/N,
-                                 (i + 1) * out_width/N, out_width, out_height,
-                                 num_samples, depth));
+        threads.emplace_back(std::thread(render_thread, camera, shapes, data,
+                                         i * out_width / N,
+                                         (i + 1) * out_width / N, out_width,
+                                         out_height, num_samples, depth));
     }
     for (int i = 0; i < N; i++) {
         threads[i].join();
@@ -134,8 +139,8 @@ Vec3 Renderer::illuminance(const IntersectionOut &surface, int max_depth,
 
     // Else pick random vector according to material
     // TODO: Implement BRDF
-    Ray wi = surface.hit_mat->sample_wi(surface.w0, surface.point, surface.normal,
-                                          random_generator);
+    Ray wi = surface.hit_mat->sample_wi(surface.w0, surface.point,
+                                        surface.normal, random_generator);
     if (wi.direction == Vec3(0, 0, 0))
         return Le;
 
@@ -145,16 +150,23 @@ Vec3 Renderer::illuminance(const IntersectionOut &surface, int max_depth,
     Vec3 Li = Vec3(0, 0, 0);
     Vec3 Fr = surface.hit_mat->Fr(wi, surface.w0, surface.normal);
 
+    // Russian Roulette threshold
+    // TODO: Try using approximated variance to calculate
+    //
+    // Use larger number of samples to remove "sparkles"
+    float p = std::max(Fr.x, std::max(Fr.y, Fr.z));
+
     // Calculate luminance of hit point else assume no light
     if (details.hit) {
+        // Darker light -> More chance of skipping
+        if (random_generator.GenerateUniformFloat() > p)
+            return Le;
         Li = illuminance(details, max_depth - 1, shapes, random_generator);
-    }
-    else
-    {
+    } else {
         Li = Vec3(0.1f, 0.5f, 0.9f);
     }
-    
-    Vec3 Lr = Fr * Li;
+
+    Vec3 Lr = Fr * Li / p;
 
     // Return monte-carlo sample
     return Le + Lr;
